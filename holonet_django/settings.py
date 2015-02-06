f79c0bf97a62ee70679cd532ee75e5a12b806423
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2015-02-06 15:13:21 +0100

    Change settings loader

--- holonet_django/settings.py
+++ holonet_django/settings.py
@@ -12,15 +12,22 @@ class Settings(object):
             'sender_blacklist': '/sender-blacklist/',
             'domain_blacklist': '/domain-blacklist/',
         },
-        'TESTING': 'test' in sys.argv
+        'TESTING': 'test' in sys.argv,
+        'RECIPIENT_MODEL': settings.AUTH_USER_MODEL,
+        'RECIPIENT_UNIQUE_IDENTIFIER_FIELD': 'pk',
+        'RECIPIENT_EMAIL_FIELD': 'email'
     }
 
     def __getattr__(self, item):
-        asked_attribute = self.CONFIGS.get(item, None)
-        if asked_attribute is None:
-            asked_attribute = getattr(settings, 'HOLONET_%s' % item, None)
-            if asked_attribute is None:
-                raise AttributeError('Please set HOLONET_%s in your project settings.' % item)
-        return asked_attribute
+        default_value = self.CONFIGS.get(item, None)
+        settings_value = getattr(settings, 'HOLONET_%s' % item, None)
+
+        if settings_value is not None:
+            return settings_value
+
+        if default_value is not None:
+            return default_value
+
+        raise AttributeError('Please set HOLONET_%s in your project settings.' % item)
 
 holonet_settings = Settings()
